0e3d7dbc68e6d2f628dfd8d527406b8b4adf566e
Bharatgupta922 <gbharat922@gmail.com>
2022-03-09 21:10:49 +0530

    monetised project added to portfolio website

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,15 @@
 export const projects = [
   {
+    title: "New Era Global Pvt. Ltd.",
+    description:
+      "The company is known for supplying dyes, chemicals, oxidizers for fabrics. A monetised Project which i have developed on demand for Aadinath Infosystems",
+    image: "/images/5.png",
+    tags: ["React","Nextjs","Bootstrap","Firebase"],
+    source: "https://drive.google.com/file/d/1wy3gpYVax6u4jLK00p56bQ2lhoNKqNui/view?usp=sharing",
+    visit: "http://neweraglobalpvtltd.com/",
+    id: 0,
+  },
+  {
     title: "CMS Blog",
     description:
       "Using React, GraphQl , Tailwind & NextJs I have made an blogging website - from start to finish. The Web App is called CMS Blog and it is a simple social media Web app that allows users to post interesting events related to IT that happened in their lives.",
@@ -7,7 +17,7 @@ export const projects = [
     tags: ["GraphQl", "Tailwind", "React", "Next"],
     source: "https://github.com/Bharatgupta922/graphcms_blog",
     visit: "https://graphcms-blog-ruby.vercel.app/",
-    id: 0,
+    id: 1,
   },
   {
     title: "Chitkara ACM",
@@ -17,7 +27,7 @@ export const projects = [
     tags: ["React", "JavaScript", "firebase", "notifiy"],
     source: "https://github.com/Bharatgupta922/acm_site/tree/main",
     visit: "https://acm.chitkara.edu.in/",
-    id: 1,
+    id: 2,
   },
   {
     title: "CryptoVerse",
@@ -27,7 +37,7 @@ export const projects = [
     tags: ["React", "Redux"],
     source: "https://github.com/Bharatgupta922/CryptoVerse",
     visit: "https://github.com/Bharatgupta922/CryptoVerse",
-    id: 2,
+    id: 3,
   },
   {
     title: "PotShop",
@@ -37,7 +47,7 @@ export const projects = [
     tags: ["React", "Redux", "Firebase"],
     source: "https://github.com/Bharatgupta922/potshop-",
     visit: "https://potshop-da2d9.web.app/",
-    id: 3,
+    id: 4,
   },
 ];
 
